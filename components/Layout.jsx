b31d74355e760111f974dca1daa4b8a021ef5b8f
Issmaa <isma_h21@outlook.com>
2022-10-13 23:41:59 -0500

    last changes

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>JS Masstery Store</title>
+      </Head>
+      <header>
+        <Navbar/>
+        <main className="main-container">
+        {children}
+        </main>
+      </header>
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
